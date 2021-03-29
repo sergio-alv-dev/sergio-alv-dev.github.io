cf9f471adc7359438cfc78d152ccc89a07afd174
sergio-alv-dev <sergio.alvarez.42@gmail.com>
2021-03-29 16:15:53 -0300

    Modificada descripción curso socket.io

--- src/person.js
+++ src/person.js
@@ -16,11 +16,26 @@ export const sergio = {
       {name: 'linkedin', url: 'https://www.linkedin.com/in/sergio-alvarez-388973144'}
     ],
     experience: [
-      {jobTitle: 'Práctica Profesional 2', company: 'SALFA', startDate: 'Jan 2019', endDate: 'Mar 2019', jobDescription: 'Mi periodo de práctica se basó en el manejo de la plataforma de colaboración empresarial Sharepoint, ahí debí crear un sitio para el Area de informática que sirviera de repositorio de documentos, manejando permisos de usuarios.'},
-      {jobTitle: 'Práctica Profesional 1', company: 'Canal 13 SPA', startDate: 'Jan 2018', endDate: 'Mar 2018', jobDescription: 'Durante las primeras dos semanas aprendí y realicé trabajos en el area de soporte, para luego pasar al area de desarrollo a realizar pruebas en modulos legacy que pasarían a la nube en OracleCloud.'}
+      {
+        jobTitle: 'Práctica Profesional 2',
+        company: 'SALFA',
+        startDate: 'Jan 2019',
+        endDate: 'Mar 2019', 
+        jobDescription: 'Mi periodo de práctica se basó en el manejo de la plataforma de colaboración empresarial Sharepoint, ahí debí crear un sitio para el Area de informática que sirviera de repositorio de documentos, manejando permisos de usuarios.'},
+      {
+        jobTitle: 'Práctica Profesional 1', 
+        company: 'Canal 13 SPA', 
+        startDate: 'Jan 2018', 
+        endDate: 'Mar 2018', 
+        jobDescription: 'Durante las primeras dos semanas aprendí y realicé trabajos en el area de soporte, para luego pasar al area de desarrollo a realizar pruebas en modulos legacy que pasarían a la nube en OracleCloud.'}
     ],
     education: [
-      {degree: 'Ingeniería Civil Informática', institution: 'PUCV', startDate: 'Jan 2014', endDate: 'Dec 2020', description: 'Para mi tesis utilicé redes neuronales recurrentes LSTM. Librerías para el manejo y muestra de datos ( Pandas, Numpy, Matplot ), el procesamiento de datos ( TensorFlow, Keras ), y mi favorita, T_tides la cual me entregaba el análisis armónico de la marea. Todo esto con objeto de obtener los futuros niveles de la marea, en base a los datos entregados por la Intergovernmental Oceanographic Commission - Sea Level Station Monitoring Facility '},
+      {
+        degree: 'Ingeniería Civil Informática', 
+        institution: 'PUCV', 
+        startDate: 'Jan 2014', 
+        endDate: 'Dec 2020', 
+        description: 'Para mi tesis utilicé redes neuronales recurrentes LSTM. Librerías para el manejo y muestra de datos ( Pandas, Numpy, Matplot ), el procesamiento de datos ( TensorFlow, Keras ), y mi favorita, T_tides la cual me entregaba el análisis armónico de la marea. Todo esto con objeto de obtener los futuros niveles de la marea, en base a los datos entregados por la Intergovernmental Oceanographic Commission - Sea Level Station Monitoring Facility '},
 
       // http://www.ioc-sealevelmonitoring.org/service.php
 
@@ -33,24 +48,53 @@ export const sergio = {
     ],
     certificate: [
       // 
-      {name: 'Scrum Fundamentals', institution:  'SCRUMstudy', date: 'Jan 2019', description: 'Certificación obtenida en el curso de Ingeniería de Proyectos.'},
+      {
+        name: 'Scrum Fundamentals', 
+        institution:  'SCRUMstudy', 
+        date: 'Jan 2019', 
+        description: 'Certificación obtenida en el curso de Ingeniería de Proyectos.'},
       // 
-      {name: 'React: De cero a experto ( Hooks y MERN )', institution: 'Udemy', duration : 45, date: 'Jan 2021', description: 'Realizado esencialmente para reforzar el area de pruebas, se estudió el stack MERN, reflejado en aplicaciones que hacen uso del contenedor de estado REDUX, CRUD en firebase y Mongo.' },
+      {
+        name: 'React: De cero a experto ( Hooks y MERN )',
+        institution: 'Udemy', 
+        duration : 45, 
+        date: 'Jan 2021', 
+        description: 'Realizado esencialmente para reforzar el area de pruebas, se estudió el stack MERN, reflejado en aplicaciones que hacen uso del contenedor de estado REDUX, CRUD en firebase y Mongo.' },
       // 
     ],
     courses:[
       // 
-      {name: 'React: aplicaciones en tiempo real con Socket.io', duration: 16, institution:  'Udemy', description: 'Curso realizado para aprender los conceptos de WEBSOCKET y SOCKET.IO de manera más profunda, así como mapas en tiempo real.' },
+      {
+        name: 'React: aplicaciones en tiempo real con Socket.io', 
+        duration: 16, 
+        institution:  'Udemy', 
+        description: 'Curso realizado para aprender los conceptos de WEBSOCKET y SOCKET.IO de manera más profunda, CRUD basado en sockets, así como la sincronización de mapas en tiempo real.' },
       // 
-      {name: 'Master en Python 3.x. Aprende de cero a experto con Práctica.', duration: 5, institution:  'Udemy', description: 'Curso realizado para fortalecer conceptos de errores y manejo de excepciones y revisión de buenas prácticas.' },
+      {
+        name: 'Master en Python 3.x. Aprende de cero a experto con Práctica.', 
+        duration: 5, 
+        institution:  'Udemy', 
+        description: 'Curso realizado para fortalecer conceptos de errores y manejo de excepciones y revisión de buenas prácticas.' },
       // 
-      {name: 'Node: de cero a experto', duration: 12.5 , description: 'Desde las bases de node, manejo de servicio REST con librerías como EXPRESS, AXIOS, hasta una aplicación de colas y un chat basados en sockets con SOCKET.IO'},
+      {
+        name: 'Node: de cero a experto', 
+        duration: 12.5 , 
+        description: 'Desde las bases de node, manejo de servicio REST con librerías como EXPRESS, AXIOS, hasta una aplicación de colas y un chat basados en sockets con SOCKET.IO'},
       // 
-      {name: 'Redux en Angular con NGRX',duration: 12, description: 'Manejo del STORE, EFFECTS, ACTIONS, DEVTOOLS, lazyload, login y buenas prácticas en Angular'},
+      {
+        name: 'Redux en Angular con NGRX',
+        duration: 12, 
+        description: 'Manejo del STORE, EFFECTS, ACTIONS, DEVTOOLS, lazyload, login y buenas prácticas en Angular'},
       // 
-      {name: 'Git GitHub: Curso Control de Versiones', duration: 7, description: 'Curso realizado para pulir el manejo de control de versiones, nivel avanzado'},
+      {
+        name: 'Git GitHub: Curso Control de Versiones', 
+        duration: 7, 
+        description: 'Curso realizado para pulir el manejo de control de versiones, nivel avanzado'},
       // 
-      {name: 'Docker Curso Intensivo para DevOps y Desarrolladores', duration: 3.30, description: 'Curso realizado para aprender el pipeline de integración continua en Docker'},
+      {
+        name: 'Docker Curso Intensivo para DevOps y Desarrolladores', 
+        duration: 3.30, 
+        description: 'Curso realizado para aprender el pipeline de integración continua en Docker'},
 
     ],
     skills: [
